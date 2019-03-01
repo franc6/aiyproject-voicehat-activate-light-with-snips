831031f1cc912dc56b44ea77de5d935a279442ca
Thomas Francis Jr <franc@tcsaf.com>
2019-03-01 08:17:26 -0500

    Added code to make the button fake the snips hotword detection
    This means if you press the button, you can then speak a command to
    snips, without actually speaking the hotword.

--- snips.lights.py
+++ snips.lights.py
@@ -1,9 +1,10 @@
-import sys
-import os
-import time
+"""Quick python script to turn the LED of a Google aiyproject-voicehat
+on or off when snips is listening."""
+from functools import partial
 import json
 
-# If you don't bother to install the python module, uncomment the next line
+# If you didn't install the python module, uncomment the next two lines
+#import sys
 #sys.path.append('/home/pi/aiyprojects-raspbian/src/')
 import aiy.voicehat
 import RPi.GPIO
@@ -16,44 +17,88 @@ PORT = 1883
 
 # Place your satellite's siteId here.  See bind setting in the
 # [snips-audio-server] section of snips.toml.  Don't include the @mqtt part!
-THIS_SITE="default"
+THIS_SITE = "default"
 
 mqtt.Client.light_is_on = False
+mqtt.Client.is_connected = False
 
 RPi.GPIO.setwarnings(False)
-led = aiy.voicehat.get_led()
+LED = aiy.voicehat.get_led()
+BUTTON = aiy.voicehat.get_button()
+
+def button_pressed(client):
+    """Fakes hotword detection for snips"""
+    if client.is_connected:
+        hey_snips = {"siteId": THIS_SITE, "modelId":"hey_snips", "modelVersion": "hey_snips-3.1_2018-04-13T15:27:35_model_0019", "modelType": "universal", "currentSensitivity": 0.5}
+        payload = json.dumps(hey_snips)
+        client.publish("hermes/hotword/default/detected", payload)
 
 def light_on(client):
+    """Turns the LED on if it's not already on"""
     if client.light_is_on is False:
-        led.set_state(aiy.voicehat.LED.ON)
+        LED.set_state(aiy.voicehat.LED.ON)
         client.light_is_on = True
 
 def light_off(client):
+    """Turns the LED off if it's on"""
     if client.light_is_on is True:
-        led.set_state(aiy.voicehat.LED.OFF)
+        LED.set_state(aiy.voicehat.LED.OFF)
         client.light_is_on = False
 
 def start_listening(client, userdata, msg):
+    """hermes/asr/startListening handler
+
+    This is called when the hermes/asr/startListening intent is received.
+    It first checks that the intent is for THIS_SITE, and if so, it
+    turns the LED on.
+    """
+    del userdata
     payload = json.loads(msg.payload.decode('utf-8'))
-    if payload is not None and payload.get('siteId') is not None and payload.get('siteId') == THIS_SITE:
+    if payload is not None and \
+       payload.get('siteId') is not None and \
+       payload.get('siteId') == THIS_SITE:
         light_on(client)
 
 def stop_listening(client, userdata, msg):
+    """hermes/asr/stopListening handler
+
+    This is called when the hermes/asr/stopListening intent is received.
+    It first checks that the intent is for THIS_SITE, and if so, it
+    turns the LED off.
+    """
+    del userdata
     payload = json.loads(msg.payload.decode('utf-8'))
-    if payload is not None and payload.get('siteId') is not None and payload.get('siteId') == THIS_SITE:
+    if payload is not None and \
+       payload.get('siteId') is not None and \
+       payload.get('siteId') == THIS_SITE:
         light_off(client)
 
 def on_connect(client, userdata, flags, rc):
-    #print("Connected with result code" + str(rc))
-    client.subscribe("hermes/asr/stopListening")
-    client.subscribe("hermes/asr/startListening")
+    """MQTT Client connect callback.
 
-client = mqtt.Client()
-client.on_connect = on_connect
+    This is invoked when the MQTT client connects to the server.  It
+    then subscribes to the interesting intents
+    """
+    del userdata, flags
+    if rc == 0:
+        client.is_connected = True
+        client.subscribe("hermes/asr/stopListening")
+        client.subscribe("hermes/asr/startListening")
 
-client.message_callback_add("hermes/asr/stopListening", stop_listening)
-client.message_callback_add("hermes/asr/startListening", start_listening)
+def main():
+    """Main entry point"""
+    LED.set_state(aiy.voicehat.LED.OFF)
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    button_pressed_partial = partial(button_pressed, client)
+    BUTTON.on_press(button_pressed_partial)
 
-client.light_is_on = False
-client.connect(HOST, PORT, 60)
-client.loop_forever()
+    client.message_callback_add("hermes/asr/stopListening", stop_listening)
+    client.message_callback_add("hermes/asr/startListening", start_listening)
+
+    client.light_is_on = False
+    client.connect(HOST, PORT, 60)
+    client.loop_forever()
+
+if __name__ == "__main__":
+    main()
